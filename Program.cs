480632cd0dc4095bd8071ae6892b10a5376c1c4c
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 17:24:04 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("--- Decomposição Decimal ---\n");
+
+        Console.Write("Digite um número inteiro...: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int unidades = numero % 10;
+        int dezenas = (numero / 10) % 10;
+        int centenas = (numero / 100) % 10;
+
+        Console.WriteLine("\nO número " + numero + " possui:");
+        Console.WriteLine("         " + unidades + " unidade(s)");
+        Console.WriteLine("         " + dezenas + " dezena(s)");
+        Console.WriteLine("        " + centenas + " centena(s).");
+    }
+}
